b11fd2df5a4828b6806c2808fae1b016815f394b
codepreacher <75647192+code-preacher@users.noreply.github.com>
2020-12-16 00:52:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -47,8 +47,6 @@ if(isset($_POST['submit'])){
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
 
-	<link rel="stylesheet" href="bootstrap/bootstrap.min.css"/>
-
 </head>
 <body>
 
@@ -210,13 +208,6 @@ if(isset($_POST['submit'])){
 
 			</form>
 		</div>
-
-		<script type="text/javascript">
-			
-		</script>
-
-		<script src="bootstrap/jquery.min.js"></script>
-		<script src="bootstrap/bootstrap.min.js"></script>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
